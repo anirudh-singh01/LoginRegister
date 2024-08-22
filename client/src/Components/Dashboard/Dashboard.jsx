a84041313db42296eea55acf28c21a9474a31d57
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-08-22 22:02:26 +0530

    added dashboard

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,12 +1,53 @@
-import Sidebar from "./Sidebar/Sidebar"
-import Content from "./Content/Content"
+// import Sidebar from "./Sidebar/Sidebar"
+// import Content from "./Content/Content"
+import { useState } from 'react';
+import { Button, Layout, theme } from 'antd';
+import Logo from '../Logo';
+import MenuList from '../MenuList';
+import ToggleThemeButton from '../ToggleThemeButton';
+
+// Destructure Layout components
+const { Header, Sider } = Layout;
 
 const Dashboard = () => {
+    // State to manage theme (dark or light) and sidebar collapse state
+    const [darkTheme, setDarkTheme] = useState(true);
+    const [collapsed, setCollapsed] = useState(false);
+
+    // Toggle theme between dark and light
+    const toggleTheme = () => {
+        setDarkTheme(!darkTheme);
+    };
+
+    // Retrieve color background for the header from Ant Design theme tokens
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
     return (
-        <div className="app">
-            <Sidebar />
-            <Content />
-        </div>
+        <>
+          <Layout>
+            {/* Sidebar (Sider) */}
+            <Sider
+                collapsed={collapsed}
+                collapsible
+                trigger={null}
+                theme={darkTheme ? 'dark' : 'light'}
+                className="sidebar"
+            >
+                {/* Logo component */}
+                <Logo />
+
+                <h2 className='title' style={collapsed ? {opacity:0} : {opacity:1}}>CIDC Demo Platform</h2>
+
+                {/* MenuList component with theme prop */}
+                <MenuList darkTheme={darkTheme} />
+          
+                {/* Button to toggle the theme */}
+                <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme} />
+            </Sider>
+          </Layout>  
+        </>
     )
 }
 
---
+++ client/src/Components/Logo.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { FireFilled } from '@ant-design/icons';
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <div className="logo-icon">
+        {/* <FireFilled /> */}
+        <img src="/getsitelogo.png" alt="Logo" style={{width:"50px"}}/>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
